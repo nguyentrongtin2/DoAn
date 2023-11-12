9b4cdabbfa184fee7614d512e8e7b6d54590cd1a
nghieee <145263187+nghieee@users.noreply.github.com>
2023-11-12 16:30:18 +0700

    Add project files.

---
+++ Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DATA.EF;
+using DATA;
+
+namespace DoAn.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            mapAccount map = new mapAccount();
+            var account = map.Find(email, password);
+            //1. Có thì sang trang admin dashboard
+            if (account != null)
+            {
+
+            }
+            return View();
+        }
+    }
+}
---
+++ Controllers/CollectionsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn.Controllers
+{
+    public class CollectionsController : Controller
+    {
+        // GET: Collections
+        public ActionResult Lich2024()
+        {
+            return View();
+        }
+
+        public ActionResult Page2() 
+        {
+            return View();
+        }
+    }
+}
